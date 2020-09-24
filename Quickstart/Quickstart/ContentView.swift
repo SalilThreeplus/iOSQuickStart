b616ec91af8e8129ad821ce4e88c75d959792012
SalilLuley <salil.luley@gmail.com>
2020-09-24 17:06:27 +0530

    Initial commit

---
+++ Quickstart/Quickstart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quickstart
+//
+//  Created by Salil on 24/09/20.
+//  Copyright © 2020 Salil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        HStack(alignment: .bottom, spacing: 10) {
+            ContentView()
+            
+        }
+    }
+}
